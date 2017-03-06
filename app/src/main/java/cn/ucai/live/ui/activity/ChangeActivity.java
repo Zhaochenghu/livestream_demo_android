11e336e49c4d2e0c2058f1c4f4f5eaa6a033bfa9
Administrator <425319874@qq.com>
2017-03-06 18:12:08 +0800

    添加账户余额界面

--- app/src/main/java/cn/ucai/live/ui/activity/ChangeActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/ChangeActivity.java
@@ -2,17 +2,31 @@ package cn.ucai.live.ui.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.live.R;
 
 /**
  * Created by Administrator on 2017/3/6.
  */
 public class ChangeActivity extends BaseActivity {
+    @BindView(R.id.tv_change_balance)
+    TextView mTvChangeBalance;
+    @BindView(R.id.target_layout)
+    LinearLayout mTargetLayout;
+    View loadingview;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change);
+        ButterKnife.bind(this);
+        loadingview = LayoutInflater.from(ChangeActivity.this).inflate(R.layout.rp_loading, null);
+        mTargetLayout.addView(loadingview);
 
     }
 }
